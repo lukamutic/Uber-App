93bc4705a64ff4b216a10e21c0e10d4984c34534
lukamutic <ml@sajtic.rs>
2022-03-15 20:33:15 +0100

    bla bla

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,20 @@
 $( document ).ready(function() {
    
 $("#sign-up-form").validate({
-    rules:{
-        username: {
-            minlength: 2
-        }
-    },
-    messages: {
-        required: "Obavezno polje!",
-        minlength: "Please, at least 2"
-    },
+    // rules:{
+    //     username: {
+    //         minlength: 2
+    //     }
+    // },
+    // messages: {
+    //     required: "Obavezno polje!",
+    //     minlength: "Please, at least 2"
+    // },
     submitHandler: function(form){
-        form.submit();
+        form.submit(); 
     }
 })
 
-
-function handleSubmit(event) {
-//     event.preventDefault();
-
-//     const data = new FormData(event.target);
-
-//     const value = Object.fromEntries(data.entries());
-
-//    value.topics = data.getAll("topics");
-
-//     console.log({ value });
-
-var formData = $("#sign-up-form").serializeObject();
-console.log(formData);
-
-  }
-
-  const form = document.querySelector("form");
-  form.addEventListener("submit", handleSubmit());
 
 
 });
