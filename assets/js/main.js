37463112f44477ea4d30223ea874d7013201fba7
lukamutic <ml@sajtic.rs>
2022-03-22 00:34:15 +0100

    added maps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,42 @@
 $( document ).ready(function() {
    
-$("#sign-up-form").validate({
-    // rules:{
-    //     username: {
-    //         minlength: 2
-    //     }
-    // },
-    // messages: {
-    //     required: "Obavezno polje!",
-    //     minlength: "Please, at least 2"
-    // },
-    submitHandler: function(form){
-        form.submit(); 
-    }
+// $("#sign-up-form").validate({
+//     // rules:{
+//     //     username: {
+//     //         minlength: 2
+//     //     }
+//     // },
+//     // messages: {
+//     //     required: "Obavezno polje!",
+//     //     minlength: "Please, at least 2"
+//     // },
+//     submitHandler: function(form){
+//         form.submit(); 
+//     }
+// })
+
+// MAPS 
+
+var map = $("#maps-col");
+var findBtn = $("#find-btn");
+var mapsBtn = $("#maps-col .primary-button");
+var mapForms = $("#map-form-col");
+
+map.hide();
+
+findBtn.on("click", function(){
+map.show(1000);
+});
+
+mapsBtn.on("click", function(){
+    mapForms.hide(800);
+    setTimeout(function() { 
+        mapForms.addClass("d-none");
+        map.removeClass("col-lg-6");
+    }, 799);
+   
 })
+
 
 
 
